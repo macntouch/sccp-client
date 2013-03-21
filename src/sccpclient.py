41374e7051f0f39d8e3f0388d5d09101a99ebeb7
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-03-21 15:17:34 -0400

    Some white spaces to be more pep8 compliant

--- src/sccpclient.py
+++ src/sccpclient.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-""" 
+"""
 Sample PyQt GUI using a Twisted-based socket client. Demonstrates how to make
 PyQt work in unison with Twisted.
 
 Eli Bendersky (eliben@gmail.com)
 This code is in the public domain
 """
-import sys, time
+import sys
+import time
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -18,21 +20,22 @@ from gui.ActorView import ActorView
 
 SERVER_HOST = '192.168.30.83'
 SERVER_PORT = 2000
-DEVICE_NAME1= 'SEP00164697AAAA'
-DEVICE_NAME2= 'SEP00164697AAAB'
-DEVICE_NAME3= 'SEP00164697AAAC'
+DEVICE_NAME1 = 'SEP00164697AAAA'
+DEVICE_NAME2 = 'SEP00164697AAAB'
+DEVICE_NAME3 = 'SEP00164697AAAC'
+
 
 class SCCPClientWindow(QMainWindow):
+
     def __init__(self, reactor, parent=None):
         super(SCCPClientWindow, self).__init__(parent)
-        self.phoneViews=[]
+        self.phoneViews = []
         self.reactor = reactor
-        
+
         self.create_main_frame()
         self.createPhones()
         self.createIndicatorTimer()
-        
- 
+
     def create_main_frame(self):
 
         mainBox = QVBoxLayout()
@@ -40,66 +43,61 @@ class SCCPClientWindow(QMainWindow):
         mainBox.addLayout(self.phoneBox)
         self.log_widget = LogWidget()
         mainBox.addWidget(self.log_widget)
-       
+
         main_frame = QWidget()
         main_frame.setLayout(mainBox)
         main_frame.setMinimumWidth(300)
-        
+
         self.setCentralWidget(main_frame)
-        
+
     def createPhones(self):
         self.createPhone(DEVICE_NAME1)
         self.createPhone(DEVICE_NAME2)
         self.createPhone(DEVICE_NAME3)
-                
-    def createPhone(self,deviceName):
-        
-        
-        mainPhoneView = PhoneView(SERVER_HOST,deviceName,self.onConnect)
+
+    def createPhone(self, deviceName):
+        mainPhoneView = PhoneView(SERVER_HOST, deviceName, self.onConnect)
         self.phoneBox.addLayout(mainPhoneView)
 
         callActor = CallActor()
         actorView = ActorView(callActor)
         mainPhoneView.addLayout(actorView)
 
-        sccpPhone = SCCPPhone(SERVER_HOST,deviceName)
+        sccpPhone = SCCPPhone(SERVER_HOST, deviceName)
         sccpPhone.setLogger(self.log)
         sccpPhone.setTimerProvider(self)
         callActor.setPhone(sccpPhone)
         callActor.setTimerProvider(self)
         sccpPhone.addCallHandler(callActor)
-        
+
         mainPhoneView.useSccpPhone(sccpPhone)
         self.phoneViews.append(mainPhoneView)
-        
-        
-    def createOneShotTimer(self,timerInSec,timerHandler):
+
+    def createOneShotTimer(self, timerInSec, timerHandler):
         self.log('timer request sec : ' + `timerInSec`)
         oneTimer = QTimer(self)
         oneTimer.setSingleShot(True)
         oneTimer.timeout.connect(timerHandler)
-        oneTimer.start(timerInSec*1000)
+        oneTimer.start(timerInSec * 1000)
 
-        
     def createIndicatorTimer(self):
         self.circle_timer = QTimer(self)
         self.circle_timer.timeout.connect(self.onIndicatorTimer)
         self.circle_timer.start(25)
-        
+
     def onIndicatorTimer(self):
         for phoneView in self.phoneViews:
             phoneView.connectIndicator.next()
-            
-    def onConnect(self,serverHost,deviceName,networkClient):
+
+    def onConnect(self, serverHost, deviceName, networkClient):
         self.log("trying to connect to : "+serverHost+ " on " +`SERVER_PORT`)
         self.connection = self.reactor.connectTCP(serverHost, SERVER_PORT, networkClient)
 
-    
-    def createTimer(self,intervalSecs,timerCallback):
+    def createTimer(self, intervalSecs, timerCallback):
         self.keepalive_timer = QTimer(self)
         self.keepalive_timer.timeout.connect(timerCallback)
-        self.keepalive_timer.start(intervalSecs*1000)
-    
+        self.keepalive_timer.start(intervalSecs * 1000)
+
     def log(self, msg):
         timestamp = '[%010.3f]' % time.clock()
         self.log_widget.append(timestamp + ' ' + str(msg))
@@ -107,21 +105,20 @@ class SCCPClientWindow(QMainWindow):
     def closeEvent(self, e):
         self.log("close event")
         self.reactor.stop()
-        
 
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
+
     try:
         import qt4reactor
     except ImportError:
         # Maybe qt4reactor is placed inside twisted.internet in site-packages?
         from twisted.internet import qt4reactor
     qt4reactor.install()
-    
+
     from twisted.internet import reactor
     mainwindow = SCCPClientWindow(reactor)
     mainwindow.show()
-    
+
     reactor.run()
